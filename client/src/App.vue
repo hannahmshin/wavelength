aab30a87ced4ee152952f881d9443fa157cdbb4b
Matthew Shin <mshin@rmn.com>
2020-01-21 00:58:53 -0600

    wip: initial client implementation

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Wavelength Logo" src="./assets/logo.png" />
+    <Player msg="Welcome to Wavelength" />
+  </div>
+</template>
+
+<script>
+import Player from './components/Player.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Player
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+//import VueRouter from "vue-router"
+
+import App from './App.vue'
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
